219546c6a7875160f699503b2147d66b408f6910
Helton <helton.dias.rodrigues@gmail.com>
2020-02-09 18:04:10 -0300

    Transition between types/fix Liskov violation

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -7,24 +7,11 @@ class Bottles
   end
 
   def verse(number)
-    bottle_number = bottle_number_for(number)
-    next_bottle_number = bottle_number_for(bottle_number.successor)
+    bottle_number = BottleNumber.for(number)
     "#{bottle_number} of beer on the wall, ".capitalize +
     "#{bottle_number} of beer.\n"+
     "#{bottle_number.action}, "+
-    "#{next_bottle_number} of beer on the wall.\n"
-  end
-
-  #bottle factory
-  def bottle_number_for(number)
-    case number
-    when 0
-      BottleNumber0
-    when 1
-      BottleNumber1
-    else
-      BottleNumber
-    end.new(number)
+    "#{bottle_number.successor} of beer on the wall.\n"
   end
 end
 
@@ -33,6 +20,17 @@ class BottleNumber
 
   def initialize(number)
     @number = number
+  end
+
+  def self.for(number)
+   case number
+    when 0
+      BottleNumber0
+    when 1
+      BottleNumber1
+    else
+      BottleNumber
+    end.new(number)
   end
 
   def to_s
@@ -56,7 +54,7 @@ class BottleNumber
   end
 
   def successor
-    number -  1
+    BottleNumber.for(number -  1)
   end
 end
 
@@ -70,7 +68,7 @@ class BottleNumber0 < BottleNumber
   end
 
   def successor
-    99
+    BottleNumber.for(99)
   end
 end
 
